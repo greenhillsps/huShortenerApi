83db773d701abfffa79802dc6d705e2e6521e359
Jawad Farooqi <jawadfarooqi17@gmail.com>
2018-10-03 12:21:52 +0500

    bug

--- server/url/url.Redirector.js
+++ server/url/url.Redirector.js
@@ -8,41 +8,44 @@ const ipstack = require('ipstack')
 function update(id, req) {
     return new Promise((resolve, reject) => {
         try {
-            Url.findOne({ queryKey: id }).lean().exec(function (err, url) {
-                let stop = false;
-                console.log(url.features.blockIps.ips.length)
-                for (let i = 0; i < url.features.blockIps.ips.length; i++) {
-                    if (url.features.blockIps.ips[i] === req.clientIp) {
-                        stop = true;
-                    }
-                }
-            })
+            // Url.findOne({ queryKey: id }).lean().exec(function (err, url) {
+            //     let stop = false;
+            //     console.log(url.features.blockIps.ips.length)
+            //     for (let i = 0; i < url.features.blockIps.ips.length; i++) {
+            //         if (url.features.blockIps.ips[i] === req.clientIp) {
+            //             stop = true;
+            //         }
+            //     }
+            // })
             ipstack("103.209.52.44", "555ffad38e5faadd4df7aaa9b9db8141", (err, response) => {
                 if (err) {
                     console.log(err)
                     reject(err)
-                } else if (url.features.customShortUrl.locked === true && url.features.locked === true) {
-
                 }
-                else if (url.features.fourOfour.locked === true && url.features.locked === true) {
+                // else if (url.features.customShortUrl.locked === true && url.features.locked === true) {
 
-                }
-                else if (url.features.customReports.locked === true && url.features.locked === true) {
+                // }
+                // else if (url.features.fourOfour.locked === true && url.features.locked === true) {
 
-                }
-                else if (url.features.blockIps.locked === true && url.features.locked === true) {
+                // }
+                // else if (url.features.customReports.locked === true && url.features.locked === true) {
 
-                }
-                else if (url.features.enableToggle.locked === true && url.features.locked === true) {
+                // }
+                // else if (url.features.blockIps.locked === true && url.features.locked === true) {
 
-                }
-                else if (url.features.urlRedirectto.locked === true && url.features.locked === true) {
+                // }
+                // else if (url.features.enableToggle.locked === true && url.features.locked === true) {
 
-                }
-                else if (url.features.customExpiryDate.locked === true && url.features.locked === true) {
+                // }
+                // else if (url.features.urlRedirectto.locked === true && url.features.locked === true) {
 
-                }
-                else if (url.features.locked === true) {
+                // }
+                // else if (url.features.customExpiryDate.locked === true && url.features.locked === true) {
+
+                // }
+                else
+                // if (url.features.locked === true) 
+                {
                     let obj = {
                         browser: useragent.parse(req.headers['user-agent']).family ?
                             useragent.parse(req.headers['user-agent']).family : "Unknown",
@@ -65,20 +68,20 @@ function update(id, req) {
                                 console.log(err)
                                 reject(err)
                             } else if (!response) {
-                                reject("errorrrr, actual url not found")
+                                resolve("errorrrr, actual url not found")
                             } else if (response.actualUrl) {
                                 console.log("this is the response url: ", response.actualUrl);
                                 resolve(response.actualUrl)
                             } else {
-                                reject(err)
+                                resolve("errorrrr, actual url not found")
                             }
                         });
                 }
             })
         } catch (err) {
-            console.log(err)
-            reject(err)
-        }
+            console.log(err);
+            reject(err);
+        };
     })
 }
 
