d25c89f5808cab2dee6f6839f35dde076142da49
Jawad Farooqi <jawadfarooqi17@gmail.com>
2018-11-02 18:12:57 +0500

    +

--- server/PPexec/PPPexec.js
+++ server/PPexec/PPPexec.js
@@ -7,49 +7,54 @@ const User = require('../user/user.model');
 
 // success page 
 router.use('/success', (req, res) => {
-
-    var paymentId = req.query.paymentId;
-    var payerId = { 'payer_id': req.query.PayerID };
-    // console.log("This is the payment iddddddddddddddddddddddddddddddddddddddddddddd", paymentId)
-    // calling the final payment execute method
-    paypal.payment.execute(paymentId, payerId, function (error, payment) {
-        if (error) {
-            console.error(error);
-        } else {
-            if (payment.state === 'approved') {
-                // console.log(payment)
-                res.redirect('http://dotlydev.herokuapp.com/success');
-                User.findOne({ paymentId: { $all: [paymentId] } }, function (err, user) {
-                    if (err) {
-                        res.redirect('http://dotlydev.herokuapp.com/failure');
-                        console.log("User could not be updated in payment execution", err)
-                    } else {
-                        // console.log("This is the payment amount: ", payment.transactions[0].amount.total);
-                        // user.wallet += parseInt(payment.transactions[0].amount.total);
-                        // user.transactionHistory.push(payment)
-                        // console.log("This is the current user: ", user);
-                        // console.log("This is the payment object: ", payment);
-                        User.findByIdAndUpdate(user._id, {
-                            $inc: { 'wallet': parseInt(payment.transactions[0].amount.total) },
-                            $push: {
-                                transactionHistory: payment
-                            }
-                        }, { new: true }, function (err, newuser) {
-                            if (err) {
-                                res.redirect('http://dotlydev.herokuapp.com/failure');
-                                // console.log("User could not be updated in payment execution")
-                            } else {
-                                // console.log("This is the current wallet of  user: ", newuser.wallet);
-                                // console.log("This is the payment status: ", payment);
-                            }
-                        })
-                    }
-                })
+    try {
+        var paymentId = req.query.paymentId;
+        var payerId = { 'payer_id': req.query.PayerID };
+        // console.log("This is the payment iddddddddddddddddddddddddddddddddddddddddddddd", paymentId)
+        // calling the final payment execute method
+        paypal.payment.execute(paymentId, payerId, function (error, payment) {
+            if (error) {
+                console.error(error);
             } else {
-                res.redirect('http://dotlydev.herokuapp.com/failure');
+                if (payment.state === 'approved') {
+                    // console.log(payment)
+                    res.redirect('http://dotlydev.herokuapp.com/success');
+                    User.findOne({ paymentId: { $all: [paymentId] } }, function (err, user) {
+                        if (err) {
+                            res.redirect('http://dotlydev.herokuapp.com/failure');
+                            console.log("User could not be updated in payment execution", err)
+                        } else {
+                            // console.log("This is the payment amount: ", payment.transactions[0].amount.total);
+                            // user.wallet += parseInt(payment.transactions[0].amount.total);
+                            // user.transactionHistory.push(payment)
+                            // console.log("This is the current user: ", user);
+                            // console.log("This is the payment object: ", payment);
+                            User.findByIdAndUpdate(user._id, {
+                                $inc: { 'wallet': parseInt(payment.transactions[0].amount.total) },
+                                $push: {
+                                    transactionHistory: payment
+                                }
+                            }, { new: true }, function (err, newuser) {
+                                if (err) {
+                                    res.redirect('http://dotlydev.herokuapp.com/failure');
+                                    // console.log("User could not be updated in payment execution")
+                                } else {
+                                    // console.log("This is the current wallet of  user: ", newuser.wallet);
+                                    // console.log("This is the payment status: ", payment);
+                                }
+                            })
+                        }
+                    })
+                } else {
+                    res.redirect('http://dotlydev.herokuapp.com/failure');
+                }
             }
-        }
-    });
+        });
+    } catch (error) {
+        return res.json(error)
+    }
+
+
 })
 
 // error page 
