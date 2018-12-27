308381100f1f295d0eca6a64e9e316f98b4bdc60
uqair <muqairali@gmail.com>
2018-12-27 17:17:07 +0500

    url eror solved

--- server/url/urlParam-validation.js
+++ server/url/urlParam-validation.js
@@ -6,6 +6,8 @@ module.exports = {
     body: {
       actualUrl: Joi.string().required().max(2083).min(2).regex(
         /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+      ).uri(
+        
       )
     }
   },
