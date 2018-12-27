381c69c12de6f53a306308a77d2367e899c5c435
uqair <muqairali@gmail.com>
2018-12-27 18:36:25 +0500

    url eror solved

--- server/url/url.route.js
+++ server/url/url.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const urlController = require('./url.controller');
 const paramValidation = require('./urlParam-validation');
 const validate = require('express-validation');
-//const {append} =require('../helpers/helper');
+import {append} from '../helpers/helper';
 // routes
 router.post('/submit', [validate(paramValidation.createUrl), submit]);
 router.get('/search/', [validate(paramValidation.createUrl), search]);
@@ -18,14 +18,14 @@ module.exports = router;
 
 function submit(req, res, next) {
 
-// append(req.body.actualUrl).then(res=>{
-//     req.body.actualUrl=res;
+append(req.body.actualUrl).then(res=>{
+    req.body.actualUrl=res;
     urlController.create(req)
         .then(url => url ? res.json({ "url": url }) : res.status(403).json({ 'message': ' the request was not processed' }).send(403))
         .catch(err => next(err));
-// }).catch(err=>{
-//     next(err)
-// })
+}).catch(err=>{
+    next(err)
+})
     
     
 }
