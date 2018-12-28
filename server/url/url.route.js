5ee925f23ba5eedc48244163a04bf912f91bcac3
uqair <muqairali@gmail.com>
2018-12-28 10:23:03 +0500

    update

--- server/url/url.route.js
+++ server/url/url.route.js
@@ -17,7 +17,7 @@ router.put('/:id', [update]);
 module.exports = router;
 
 function submit(req, res, next) {
-   req.body.actualUrl=append(req.body.actualUrl);
+ //  req.body.actualUrl=append(req.body.actualUrl);
     urlController.create(req)
         .then(url => url ? res.json({ "url": url }) : res.status(403).json({ 'message': ' the request was not processed' }).send(403))
         .catch(err => next(err));
