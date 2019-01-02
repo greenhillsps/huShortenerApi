4c87051d88b5262d616fd5c66a9adcd08ff5ac5c
uqair <muqairali@gmail.com>
2019-01-02 12:57:55 +0500

    live paypal

--- server/Paypal/expressCheckout.js
+++ server/Paypal/expressCheckout.js
@@ -9,7 +9,7 @@ const validate = require('express-validation');
 
 // configure paypal with the credentials you got when you created your paypal app
 paypal.configure({
-    'mode': 'sandbox', //sandbox or live 
+    'mode': 'live', //sandbox or live 
     'client_id': config.clientId, // client id
     'client_secret': config.clientSecret // client secret 
 });
