a084f7d9a7f2f212614d66d60c4b84b1066fd753
uqair <muqairali@gmail.com>
2019-01-02 13:36:26 +0500

    udpate paypall working

--- server/Paypal/expressCheckout.js
+++ server/Paypal/expressCheckout.js
@@ -53,8 +53,8 @@ router.use('/buy', validate(paramValidation.paypalPay), (req, res) => {
                             "payment_method": "paypal"
                         },
                         "redirect_urls": {
-                            "return_url": "http://app.tickws.com/success",
-                            "cancel_url": "http://app.tickws.com/failure"
+                            "return_url": "http://app.tickws.com/PPexec/success",
+                            "cancel_url": "http://app.tickws.com/PPexec/failure"
                         },
                         "transactions": [{
                             "item_list": {
