3b027f2bbaec68eb965287f07f0457b57ea3e6eb
Your Name <you@example.com>
2019-09-18 00:19:01 -0700

    removeing extra stup

--- Routes/api.route.js
+++ Routes/api.route.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const userRoutes = require('../server/user/user.route');
-const adminUserRoutes = require('../server/adminUser/adminUser.route');
-const adminOpRoutes = require('../server/adminOp/adminOp.route');
 const authRoutes = require('../server/auth/auth.route');
 const urlRoutes = require('../server/url/url.route');
 const priceRoutes = require('../server/price/price.route');
-const feedbackRoutes = require('../server/feedback/feedback.route');
 const cartRoutes = require('../server/cart/cart.route');
 const featureRoutes = require('../server/feature/feature.route');
-const paypalRoutes = require('../server/Paypal/expressCheckout');
-const PPPexec = require('../server/PPexec/PPPexec');
 const VerifyToken = require('../config/VerifyToken');
 const router = express.Router(); // eslint-disable-line new-cap
 var useragent = require('express-useragent');
@@ -17,6 +12,7 @@ var useragent = require('express-useragent');
 router.use(useragent.express()); //remove this code, temporary for fikifoo
 
 // TODO: use glob to match *.route files
+
 
 /** GET /health-check - Check service health */
 router.get('/health-check', (req, res) =>
@@ -42,11 +38,7 @@ router.get('/fikifoo', (req, res) => {
 // mount auth routes on /auth
 router.use('/auth', authRoutes);
 
-// mount paypal routes on /paypal
-router.use('/paypal', [VerifyToken, paypalRoutes]);
 
-// mount paypal payment execution routes on /PPexec
-router.use('/PPexec', PPPexec);
 
 // mount users routes at /users
 router.use('/users', [VerifyToken, userRoutes]);
@@ -57,8 +49,6 @@ router.use('/url', [VerifyToken, urlRoutes]);
 // mount price routes at /price
 router.use('/price', [VerifyToken, priceRoutes]);
 
-// mount feedback routes at /feedback
-router.use('/feedback', feedbackRoutes);
 
 // mount cart routes at /cart
 router.use('/cart', [VerifyToken, cartRoutes]);
@@ -70,7 +60,5 @@ router.use('/redirect', (req, res) => {
   res.redirect('https://www.google.com.pk');
 })
 
-router.use('/adminpanel', adminUserRoutes);
 
-router.use('/adminOp', [VerifyToken, adminOpRoutes]);
 module.exports = router;
